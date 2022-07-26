cdb1be8aa79ecb2d5c59eadca9c9b894bc643350
Mir <65391345+Mir-SA@users.noreply.github.com>
2022-07-26 11:54:21 +0530

    lift doors & design changes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let addLift = document.querySelector(".addLift"),
   body = document.querySelector("body"),
   lift = document.querySelector(".lift");
 (i = 2),
-  (lDist = 1),
+  (lDist = 2),
   (currentFloor = 0),
   (lifts = 1),
   (floors = []),
@@ -13,39 +13,62 @@ let addLift = document.querySelector(".addLift"),
 
 addFloor.addEventListener("click", () => {
   currentFloor++;
+  let allFloors = document.querySelectorAll("section");
   let section = document.createElement("section");
   let floorBase = document.createElement("hr");
   let liftBtns = `
           <div class="liftBtns">
-            <button class="liftBtn ${currentFloor}">&uarr;</button>
-            <button class="liftBtn ${currentFloor}">&darr;</button>  
+           <button class="liftBtn ${currentFloor}">&uarr;</button>      
           </div>
           `;
 
   section.classList.add("floor");
   section.innerHTML = liftBtns;
+
+  if (allFloors.length > 1) {
+    console.log(allFloors);
+
+    for (let i = 0; i < allFloors.length - 1; i++) {
+      let liftBtns1 = `
+              <button class="liftBtn ${
+                allFloors.length - (i + 1)
+              }">&uarr;</button>
+              <button class="liftBtn ${
+                allFloors.length - (i + 1)
+              }">&darr;</button>
+      `;
+
+      allFloors[i].firstElementChild.innerHTML = liftBtns1;
+      // console.log(allFloors[i].firstElementChild);
+    }
+  } else {
+  }
+
   section.append(floorBase);
   main.prepend(section);
-  // console.log(floor);
+  // console.log(allFloors);
 });
 
-addLift.addEventListener(
-  "click",
-  () => {
-    lifts++;
-    let div = document.createElement("div");
-    div.classList.add("lift");
-    div.dataset.currentfloor = 0;
-    // console.log(div.style);
-    div.style.left = 5 * i + 1.5 * lDist + "rem";
-    i++;
-    lDist++;
-    console.log(i);
+addLift.addEventListener("click", () => {
+  lifts++;
+  let div = document.createElement("div");
+  div.classList.add("lift");
+  div.dataset.currentfloor = 0;
+  let doors = `
+    <div class="left door"></div>
+    <div class="right door"></div>
+  `;
+  div.innerHTML = doors;
+  div.style.left = 5 * i + 1.5 * lDist + "rem";
+  i++;
+  lDist += 2;
 
-    floor.appendChild(div);
-  },
-  true
-);
+  floor.appendChild(div);
+});
+
+// main.addEventListener("change", () => {
+//   console.log("Event on Main");
+// });
 
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("liftBtn")) {
@@ -56,29 +79,37 @@ document.addEventListener("click", (e) => {
 
 function availableLift(floorNum) {
   let allLifts = document.querySelectorAll(".lift");
-  console.log(allLifts);
+  // console.log(allLifts);
   for (let i = 0; i < allLifts.length; i++) {
     if (!allLifts[i].classList.contains("busy")) {
       moveLift(floorNum, allLifts[i]);
       return;
     }
   }
-  floors.push(floorNum);
+  // floors.push(floorNum);
+  console.log(floors);
 }
 
 function moveLift(floor, lift) {
   let currentFloor = lift.dataset.currentfloor;
   let duration = Math.abs(floor - currentFloor) * 2;
-  let move = floor * -17;
+  let move = floor * -8.5;
   lift.dataset.currentfloor = floor;
   lift.classList.add("busy");
-  lift.style.transform = "translateY(" + move + "vh)";
+  lift.style.transform = `translateY(${move}rem)`;
   lift.style.transition = `transform ${duration}s linear`;
 
   setTimeout(() => {
+    lift.children[0].classList.add("open-left-door");
+    lift.children[1].classList.add("open-right-door");
+  }, duration * 1000 + 1000);
+
+  setTimeout(() => {
+    lift.children[0].classList.remove("open-left-door");
+    lift.children[1].classList.remove("open-right-door");
+  }, duration * 1000 + 3000);
+
+  setTimeout(() => {
     lift.classList.remove("busy");
-    if (floors.length) {
-      moveLift(floors.shift(), lift);
-    }
-  }, duration * 1000 + 100);
+  }, duration * 1000 + 5000);
 }
